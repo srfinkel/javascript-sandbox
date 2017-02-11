308c434616da8bc16d6c5876075edf6f18d41bbd
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-11 07:54:15 -0700

    Added html

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<title>Playing with JS</title>
+</html>
+	<header>
+		Practicing Javascript
+	</header>
+		<body>
+			<script src="scripts/main.js"><</script>
+		</body>
+
